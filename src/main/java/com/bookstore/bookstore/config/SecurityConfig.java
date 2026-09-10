e3d03ac7bc958c19e6bc2358dc092148261f5be6
S Prithika <prithikaskv16@gmail.com>
2026-09-10 16:23:01 +0530

    Allow Vercel frontend in CORS

--- src/main/java/com/bookstore/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/bookstore/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://localhost:5175",
-                "http://localhost:5176"
+                "http://localhost:5176",
+                "https://online-book-store-frontend-sigma.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
